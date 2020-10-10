427de955a54a28da706fa9fbdd2a48251db39dda
Mark <wushu4@yahoo.com.vn>
2020-10-09 20:14:54 -0400

    Modified start scene to include name and id

--- src/StartScene.cpp
+++ src/StartScene.cpp
@@ -46,11 +46,15 @@ void StartScene::handleEvents()
 void StartScene::start()
 {
 	const SDL_Color blue = { 0, 0, 255, 255 };
-	m_pStartLabel = new Label("START SCENE!", "Consolas", 80, blue, glm::vec2(400.0f, 40.0f));
+	m_pStartLabel = new Label("STORMTROOPERS!", "Consolas", 80, blue, glm::vec2(400.0f, 40.0f));
 	m_pStartLabel->setParent(this);
 	addChild(m_pStartLabel);
 
-	m_pInstructionsLabel = new Label("Press 1 to Play", "Consolas", 40, blue, glm::vec2(400.0f, 120.0f));
+	m_pInstructionsLabel = new Label("Student 1: (Mark) Phu Pham Gia, 101250748", "Consolas", 20, blue, glm::vec2(400.0f, 120.0f));
+	m_pInstructionsLabel->setParent(this);
+	addChild(m_pInstructionsLabel);
+
+	m_pInstructionsLabel = new Label("Student 2: Justin Dela Cruz, 101127646", "Consolas", 20, blue, glm::vec2(400.0f, 150.0f));
 	m_pInstructionsLabel->setParent(this);
 	addChild(m_pInstructionsLabel);
 
